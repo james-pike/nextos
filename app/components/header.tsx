47d224467ce1161aa9f6d706937bcba18a74fe3e
James Pike <jamesandrewpike@gmail.com>
2023-10-22 22:45:32 -0400

    Add Navigation Bar

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,38 +4,8 @@ import React from 'react';
 import '/app/globals.css'
 import { Button, Navbar } from 'flowbite-react';
 
-export default function NavbarWithCTAButton() {
-  return (
-    
-    <Navbar fluid rounded className='fixed top-0 z-50 w-full'>
-      <Navbar.Brand href="https://flowbite-react.com">
-      <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
-  
-  )
-}
-
-
-
-{/*}
-
-
-const Header = () => {
+export default function Header() {
+ 
     const navbarStyle = {
         position: 'fixed',
         top: 0,
@@ -82,6 +52,3 @@ const Header = () => {
     )
 }
 
-export default Header;
-
-*/}
